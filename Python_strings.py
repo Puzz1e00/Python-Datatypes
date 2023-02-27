8575e444f5179aadcffd6c82bb8f3fcdaf3d6bb8
Puzz1e00 <baibhavacharya10@gmail.com>
2023-02-27 15:31:02 +0545

    first commit

---
+++ Python_strings.py
@@ -0,0 +1,20 @@
+a="broadway"
+# triple quoted characters are also strings
+"""
+triple quoted can be used to write comments
+"""
+a="broadway"
+b="infosys"
+#Operations
+print(a+b)#concatenation- combine
+print(a*3)#repitition
+print(a[4])#indexing
+print('b'in a)#membership
+#Built In Functions
+print(bool(''))
+c=len('Hello')#len is count
+print(c)
+strl="Hello World"
+x=slice(2,5)#slicing
+print(strl[x])
+
